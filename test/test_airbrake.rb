ccf3027be3c6f1e22bbeec1b10080bd47aef430f
ank <adamnkraut@gmail.com>
2010-10-14 15:07:49 -0400

    renamed tests

--- test/test_airbrake.rb
+++ test/test_airbrake.rb
@@ -1,7 +1,5 @@
 require 'helper'
 
-class TestDropFolders < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+class TestAirBrake < Test::Unit::TestCase
+  
 end
